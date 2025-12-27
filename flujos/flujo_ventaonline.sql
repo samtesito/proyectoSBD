7eafea701ddd690619ff3b7b2e3276ac015c2a8a
viobi <vivsk0806@gmail.com>
2025-12-26 21:31:38 -0400

    flujo venta online
    como creo que es el flujo de venta online wait

--- flujos/flujo_ventaonline.sql
+++ flujos/flujo_ventaonline.sql
@@ -1,3 +1,8 @@
 --Script Venta Online
 
+--Selecciona Cliente
+--Segun pais del cliente, se hace el catalogo para mostrar referente al pais
 --Muestra Productos
+--Selecciona Productos
+--Confirma compra
+--Generar Factura
