8adb81fb6df2d6f1430bf09266647f7950af1ede
DanielAzuaje7 <166616320+DanielAzuaje7@users.noreply.github.com>
2025-12-27 03:01:12 -0400

    Update flujo_ventaonline.sql
    Olvide hacer pull entonces estoy poniendo algunas cosas manualmente

--- flujos/flujo_ventaonline.sql
+++ flujos/flujo_ventaonline.sql
@@ -1,10 +1,15 @@
 --Script Venta Online
 
---Selecciona Cliente
---Muestra Clientes 
+--Muestra Clientes para seleccionar uno por su ID
 SELECT * FROM V_CLIENTES;
+VARIABLE id_cliente NUMBER(8);
+ACCEPT cliente_id NUMBER PROMPT 'Ingrese el ID del cliente que realiza la compra: '
+EXEC :id_cliente := &cliente_id;
+
 
 --Segun pais del cliente, se hace el catalogo para mostrar referente al pais
+
+
 --Muestra Productos
 --Selecciona Productos
 --Confirma compra
