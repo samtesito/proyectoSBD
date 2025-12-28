8f4213f0cc1a15b0155caef060df5d5b41698822
SamSam <168691193+samtesito@users.noreply.github.com>
2025-12-28 15:09:02 -0400

    Update inserts_prueba_gest_inventario.sql

--- Miscelaneos/inserts_prueba_gest_inventario.sql
+++ Miscelaneos/inserts_prueba_gest_inventario.sql
@@ -1,209 +1,69 @@
-cells:
-  - kind: 2
-    value: "------------------------------------------------------------\r
-
-      -- 1. FACTURAS_TIENDA\r
-
-      -- Facturas emitidas en distintas fechas y tiendas\r
-
-      ------------------------------------------------------------\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2001, 3001, 101, DATE '2025-12-13', 150.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2002, 3002, 101, DATE '2025-12-13', 200.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2003, 3003, 101, DATE '2025-12-13', 175.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2004, 3004, 101, DATE '2025-12-14', 220.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2005, 3005, 101, DATE '2025-12-14', 180.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2006, 3006, 102, DATE '2025-12-15', 250.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2007, 3007, 102, DATE '2025-12-15', 300.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2008, 3008, 102, DATE '2025-12-16', 190.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2009, 3009, 102, DATE '2025-12-16', 210.00);\r
-
-      INSERT INTO FACTURAS_TIENDA (nro_fact, id_cliente, id_tienda, f_emision,
-      total) VALUES (2010, 3010, 103, DATE '2025-12-17', 260.00);\r
-
-      \r
-
-      ------------------------------------------------------------\r
-
-      -- 2. LOTES_SET_TIENDA\r
-
-      -- Lotes de juguetes disponibles en tiendas\r
-
-      ------------------------------------------------------------\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (501, 101, 1, DATE '2025-11-01', 50);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (502, 101, 2, DATE '2025-11-02', 40);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (503, 101, 3, DATE '2025-11-03', 60);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (504, 101, 4, DATE '2025-11-04', 70);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (505, 102, 1, DATE '2025-11-05', 80);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (506, 102, 2, DATE '2025-11-06', 90);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (507, 102, 3, DATE '2025-11-07', 100);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (508, 102, 4, DATE '2025-11-08', 110);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (509, 103, 1, DATE '2025-11-09', 120);\r
-
-      INSERT INTO LOTES_SET_TIENDA (cod_juguete, id_tienda, nro_lote, f_adqui,
-      cant_prod) VALUES (510, 103, 2, DATE '2025-11-10', 130);\r
-
-      \r
-
-      ------------------------------------------------------------\r
-
-      -- 3. DETALLES_FACTURA_TIENDA\r
-
-      -- Detalles que vinculan facturas con juguetes y lotes\r
-
-      ------------------------------------------------------------\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2001, 1, 5, 'A', 501,
-      101, 1);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2002, 2, 3, 'M', 502,
-      101, 2);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2003, 3, 4, 'A', 503,
-      101, 3);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2004, 4, 6, 'M', 504,
-      101, 4);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2005, 5, 2, 'A', 501,
-      101, 1);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2006, 6, 7, 'M', 505,
-      102, 1);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2007, 7, 8, 'A', 506,
-      102, 2);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2008, 8, 5, 'M', 507,
-      102, 3);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2009, 9, 9, 'A', 508,
-      102, 4);\r
-
-      INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, id_det_fact, cant_prod,
-      tipo_cli, cod_juguete, id_tienda, nro_lote) VALUES (2010, 10, 4, 'M', 509,
-      103, 1);\r
-
-      \r
-
-      ------------------------------------------------------------\r
-
-      -- 4. DESCUENTOS\r
-
-      -- Registros iniciales para validar que el procedimiento inserta nuevos
-      descuentos\r
-
-      ------------------------------------------------------------\r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (501, 101, 1, 1, DATE '2025-12-13', 2);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (502, 101, 2, 2, DATE '2025-12-13', 1);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (503, 101, 3, 3, DATE '2025-12-13', 3);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (504, 101, 4, 4, DATE '2025-12-14', 2);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (505, 102, 1, 5, DATE '2025-12-15', 4);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (506, 102, 2, 6, DATE '2025-12-15', 5);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (507, 102, 3, 7, DATE '2025-12-16', 6);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (508, 102, 4, 8, DATE '2025-12-16', 7);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (509, 103, 1, 9, DATE '2025-12-17', 3);\r
-
-      \r
-
-      INSERT INTO DESCUENTOS (cod_juguete, id_tienda, nro_lote, id_desc, fecha,
-      cant) \r
-
-      VALUES (510, 103, 2, 10, DATE '2025-12-17', 2);\r\n"
-    languageId: oracle-sql
+------------------------------------------------------------
+-- 1. Poblar FACTURAS_TIENDA con 10 facturas de distintas fechas
+------------------------------------------------------------
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1001, DATE '2025-12-13', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1002, DATE '2025-12-13', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1003, DATE '2025-12-13', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1004, DATE '2025-12-13', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1005, DATE '2025-12-13', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1006, DATE '2025-12-14', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1007, DATE '2025-12-14', 1);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1008, DATE '2025-12-15', 2);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1009, DATE '2025-12-15', 2);
+INSERT INTO FACTURAS_TIENDA (nro_fact, f_emision, id_tienda) VALUES (1010, DATE '2025-12-16', 2);
+
+------------------------------------------------------------
+-- 2. Poblar DETALLES_FACTURA_TIENDA con 10 registros
+-- Cada detalle vincula una factura con un juguete, lote y cantidad
+------------------------------------------------------------
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1001, 501, 1, 2001, 5, 501);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1002, 501, 1, 2001, 3, 501);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1003, 502, 1, 2002, 4, 502);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1004, 502, 1, 2002, 6, 502);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1005, 503, 1, 2003, 2, 503);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1006, 503, 1, 2003, 7, 503);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1007, 504, 1, 2004, 8, 504);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1008, 504, 2, 2004, 5, 504);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1009, 505, 2, 2005, 9, 505);
+INSERT INTO DETALLES_FACTURA_TIENDA (nro_fact, cod_juguete, id_tienda, nro_lote, cant_prod, codigo)
+VALUES (1010, 505, 2, 2005, 4, 505);
+
+------------------------------------------------------------
+-- 3. Poblar DESCUENTOS con 10 registros iniciales
+-- Estos sirven como base para verificar que el procedimiento inserta nuevos descuentos
+------------------------------------------------------------
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 501, 2001, DATE '2025-12-13', 2);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 502, 2002, DATE '2025-12-13', 1);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 503, 2003, DATE '2025-12-14', 3);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 504, 2004, DATE '2025-12-14', 2);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 2, 504, 2004, DATE '2025-12-15', 4);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 2, 505, 2005, DATE '2025-12-15', 5);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 2, 505, 2005, DATE '2025-12-16', 2);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 501, 2001, DATE '2025-12-13', 1);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 502, 2002, DATE '2025-12-13', 2);
+INSERT INTO DESCUENTOS (id_desc, id_tienda, cod_juguete, nro_lote, fecha, cant)
+VALUES (desce.NEXTVAL, 1, 503, 2003, DATE '2025-12-14', 1);
+
+------------------------------------------------------------
+-- Con este set de datos puedes ejecutar el procedimiento
+-- y verificar que se inserten nuevos descuentos agregados
+-- según las facturas y detalles de la fecha indicada.
+------------------------------------------------------------
