1217b8d5d462a99f9f786050ecd6655d0cf65230
viobi <vivsk0806@gmail.com>
2026-01-03 00:07:16 -0400

    Reporte de Factura Online
    reporte de la factura online con powerbi

---
+++ Miscelaneos/insert_finales.sql
@@ -0,0 +1,24 @@
+//paises
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue)
+VALUES (58, 'Venezuela', 'venezolano', 'AMERICA', FALSE);
+
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue)
+VALUES (43, 'Dinamarca', 'danes', 'EUROPA', TRUE);
+
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue)
+VALUES (34, 'Espana', 'espanol', 'EUROPA', TRUE);
+
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue)
+VALUES (972, 'ISRAEL', 'israelí', 'ASIA', FALSE);
+
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue)
+VALUES (56, 'CHILE', 'chileno', 'AMERICA', FALSE);
+
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue) 
+VALUES (507, 'Panamá', 'panameño', 'AMERICA', FALSE);
+
+INSERT INTO PAISES (id, nombre, gentilicio, continente, ue) VALUES 
+(52, 'México', 'mexicano', 'AMERICA', FALSE),
+(55, 'Brasil', 'brasileño', 'AMERICA', FALSE),
+(49, 'Alemania', 'alemán', 'EUROPA', TRUE),
+(61, 'Australia', 'australiano', 'OCEANIA', FALSE);
